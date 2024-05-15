c338fb43c23ef2038c4d126968a67470b4b6fa2c
allankouidri <allan.kouidri@gmail.com>
2024-05-15 11:18:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,165 @@
-# infer_pulid
+<div align="center">
+  <img src="images/icon.png" alt="Algorithm icon">
+  <h1 align="center">infer_pulid</h1>
+</div>
+<br />
+<p align="center">
+    <a href="https://github.com/Ikomia-hub/infer_pulid">
+        <img alt="Stars" src="https://img.shields.io/github/stars/Ikomia-hub/infer_pulid">
+    </a>
+    <a href="https://app.ikomia.ai/hub/">
+        <img alt="Website" src="https://img.shields.io/website/http/app.ikomia.ai/en.svg?down_color=red&down_message=offline&up_message=online">
+    </a>
+    <a href="https://github.com/Ikomia-hub/infer_pulid/blob/main/LICENSE.md">
+        <img alt="GitHub" src="https://img.shields.io/github/license/Ikomia-hub/infer_pulid.svg?color=blue">
+    </a>    
+    <br>
+    <a href="https://discord.com/invite/82Tnw9UGGc">
+        <img alt="Discord community" src="https://img.shields.io/badge/Discord-white?style=social&logo=discord">
+    </a> 
+</p>
+
+Run PuLID, a tuning-free ID customization approach. It's is an ip-adapter alike method to restore facial identity. It uses insightface embedding, CLIP embedding and SDXL-Lightning for inferences in 4 steps. 
+
+
+![output_1](https://raw.githubusercontent.com/Ikomia-hub/infer_pulid/main/images/output_1.jpg)
+
+![illutration](https://private-user-images.githubusercontent.com/11482921/327072329-65610b0d-ba4f-4dc3-a74d-bd60f8f5ce37.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTU3NjIwODksIm5iZiI6MTcxNTc2MTc4OSwicGF0aCI6Ii8xMTQ4MjkyMS8zMjcwNzIzMjktNjU2MTBiMGQtYmE0Zi00ZGMzLWE3NGQtYmQ2MGY4ZjVjZTM3LmpwZWc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNTE1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDUxNVQwODI5NDlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0zZTAxZTNhMDJmNTJmZTUwMGVmNTNmNjliMzFhY2E5YWNhMjJjMmZiYWZkZTIyM2Q0MGI0YjAyYzgxNDMzYTBlJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.RlBFdYi8-iGmIhw6EF177HUMruadk7lznewRmEKhOcE)
+
+## :rocket: Use with Ikomia API
+
+#### 1. Install Ikomia API
+
+We strongly recommend using a virtual environment. If you're not sure where to start, we offer a tutorial [here](https://www.ikomia.ai/blog/a-step-by-step-guide-to-creating-virtual-environments-in-python).
+
+```sh
+pip install ikomia
+```
+
+#### 2. Create your workflow
+
+```python
+from ikomia.dataprocess.workflow import Workflow
+from ikomia.utils.displayIO import display
+
+# Init your workflow
+wf = Workflow()
+
+# Add algorithm
+algo = wf.add_task(name="infer_pulid", auto_connect=True)
+
+# Run on your image  
+wf.run_on(url="https://images.pexels.com/photos/4927360/pexels-photo-4927360.jpeg?cs=srgb&dl=pexels-anntarazevich-4927360.jpg&fm=jpg&w=1280&h=1920")
+
+# Inpect your result
+display(algo.get_output(0).get_image())
+```
+
+## :sunny: Use with Ikomia Studio
+
+Ikomia Studio offers a friendly UI with the same features as the API.
+- If you haven't started using Ikomia Studio yet, download and install it from [this page](https://www.ikomia.ai/studio).
+- For additional guidance on getting started with Ikomia Studio, check out [this blog post](https://www.ikomia.ai/blog/how-to-get-started-with-ikomia-studio).
+
+## :pencil: Set algorithm parameters
+- **prompt** (str): Text prompt to guide the image generation.
+- **negative_prompt** (str, *optional*) - default 'flaws in the eyes, flaws in the face, flaws, lowres, non-HDRi, low quality, worst quality,' \
+            'artifacts noise, text, watermark, glitch, deformed, mutated, ugly, disfigured, hands, ' \
+            'low resolution, partially rendered objects,  deformed or partially rendered eyes, ' \
+            'deformed, deformed eyeballs, cross-eyed, blurry'.
+The prompt not to guide the image generation. Ignored when not using guidance (i.e., ignored if `guidance_scale` is less than `1`).
+- **num_inference_steps** (int) - default '4': Number of denoising steps. 
+- **guidance_scale** (float) - default '1.2': Stable diffusion Scale for classifier-free guidance. Recommended between [1, 1.5]. 1 will be faster.
+- **guidance_scale_id** (float) - default '0.8': ID guidance scale. Recommended between [0, 5].
+- **seed** (int) - default '-1': Seed value. '-1' generates a random number between 0 and 1919655350.
+- **num_images_per_prompt** (int) - default '1': Number of generated images. 
+- **mode** (str) - default 'fidelity': Mode of the image generation 'fidelity' or 'extremely style'. We don't see much of a difference between the two.  
+- **width** (int) - default '1024': Output width. If not divisible by 8 it will be automatically modified to a multiple of 8.
+- **height** (int) - default '1024': Output height. If not divisible by 8 it will be automatically modified to a multiple of 8.
+- **id_mix** (bool) - default 'False': If you want to mix two ID image, please turn this on, otherwise, turn this off.
+
+
+```python
+from ikomia.dataprocess.workflow import Workflow
+from ikomia.utils.displayIO import display
+
+# Init your workflow
+wf = Workflow()
+
+# Set main image
+wf.set_image_input(
+    url="https://images.pexels.com/photos/4927360/pexels-photo-4927360.jpeg?cs=srgb&dl=pexels-anntarazevich-4927360.jpg&fm=jpg&w=1280&h=1920",
+    index=0
+)
+
+# Set additional image(s) [optional]
+wf.set_image_input(
+    url="https://images.pexels.com/photos/4927361/pexels-photo-4927361.jpeg?cs=srgb&dl=pexels-anntarazevich-4927361.jpg&fm=jpg&w=1280&h=1920",
+    index=1
+)
+
+wf.set_image_input(
+    url="https://images.pexels.com/photos/4927359/pexels-photo-4927359.jpeg?cs=srgb&dl=pexels-anntarazevich-4927359.jpg&fm=jpg&w=1280&h=1920",
+    index=2
+)
+
+wf.set_image_input(
+    url="https://images.pexels.com/photos/4927358/pexels-photo-4927358.jpeg?cs=srgb&dl=pexels-anntarazevich-4927358.jpg&fm=jpg&w=1280&h=1920",
+    index=3
+)
+
+# Add algorithm
+algo = wf.add_task(name="infer_pulid", auto_connect=False)
+
+# Connect inputs
+wf.connect_tasks(wf.root(), algo, [(0,0), (1,1), (2,2), (3,3)])
+
+# Set parameters
+algo.set_parameters({
+    'prompt': 'portrait, color, cinematic, in garden, soft light, detailed face, wonderwoman costum, golden boomerang tiara, short hair',
+    'guidance_scale': '1.2',
+    'guidance_scale_id': '0.8',
+    'num_inference_steps': '4',
+    'seed': '-1',
+    'width': '1024',
+    'height': '1024',
+    'mode': 'fidelity',
+    'num_images_per_prompt':'2',
+    'mix_id' : 'False'
+    })
+
+# Run your workflow
+wf.run()
+
+# Inpect your result
+display(algo.get_output(0).get_image())
+display(algo.get_output(1).get_image())
+```
+
+## :mag: Explore algorithm outputs
+
+Every algorithm produces specific outputs, yet they can be explored them the same way using the Ikomia API. For a more in-depth understanding of managing algorithm outputs, please refer to the [documentation](https://ikomia-dev.github.io/python-api-documentation/advanced_guide/IO_management.html).
+
+```python
+from ikomia.dataprocess.workflow import Workflow
+
+# Init your workflow
+wf = Workflow()
+
+# Add algorithm
+algo = wf.add_task(name="infer_pulid", auto_connect=True)
+
+# Run on your image  
+wf.run_on(url="https://images.pexels.com/photos/4927360/pexels-photo-4927360.jpeg?cs=srgb&dl=pexels-anntarazevich-4927360.jpg&fm=jpg&w=1280&h=1920")
+
+# Iterate over outputs
+for output in algo.get_outputs():
+    # Print information
+    print(output)
+    # Export it to JSON
+    output.to_json()
+```
+
+## :fast_forward: Troubleshooting 
+
+If you the installation of Insightface doesn't go smoothly on Windows. Please follow this [guide](https://github.com/Gourieff/sd-webui-reactor#insightfacebuild).
---
+++ infer_pulid_process.py
@@ -0,0 +1,230 @@
+import copy
+import numpy as np
+import torch
+from ikomia import core, dataprocess, utils
+import random
+
+from infer_pulid.PuLID.pulid import attention_processor as attention
+from infer_pulid.PuLID.pulid.pipeline import PuLIDPipeline
+from infer_pulid.PuLID.pulid.utils import resize_numpy_image_long, seed_everything
+
+# --------------------
+# - Class to handle the algorithm parameters
+# - Inherits PyCore.CWorkflowTaskParam from Ikomia API
+# --------------------
+class InferPulidParam(core.CWorkflowTaskParam):
+
+    def __init__(self):
+        core.CWorkflowTaskParam.__init__(self)
+        self.prompt = "portrait, color, cinematic, in garden, soft light, detailed face"
+        self.guidance_scale = 1.2
+        self.guidance_scale_id = 0.8
+        self.negative_prompt = 'flaws in the eyes, flaws in the face, flaws, lowres, non-HDRi, low quality, worst quality,' \
+            'artifacts noise, text, watermark, glitch, deformed, mutated, ugly, disfigured, hands, ' \
+            'low resolution, partially rendered objects,  deformed or partially rendered eyes, ' \
+            'deformed, deformed eyeballs, cross-eyed, blurry'
+        self.num_inference_steps = 4
+        self.num_images_per_prompt = 1
+        self.seed = -1
+        self.width = 1024
+        self.height = 1024
+        self.mode = 'fidelity'
+        self.id_mix = False
+        self.update = False
+
+    def set_values(self, params):
+        self.prompt = params["prompt"]
+        self.guidance_scale = float(params["guidance_scale"])
+        self.guidance_scale_id = float(params["guidance_scale_id"])
+        self.negative_prompt = params["negative_prompt"]
+        self.seed = int(params["seed"])
+        self.num_inference_steps = int(params["num_inference_steps"])
+        self.num_images_per_prompt = int(params["num_images_per_prompt"])
+        self.width = int(params["width"])
+        self.height = int(params["height"])
+        self.mode = str(params['mode'])
+        self.id_mix = utils.strtobool(params['id_mix'])
+        self.update = True
+
+    def get_values(self):
+        # Send parameters values to Ikomia Studio or API
+        # Create the specific dict structure (string container)
+        param_map = {}
+        param_map["prompt"] = str(self.prompt)
+        param_map["guidance_scale"] = str(self.guidance_scale)
+        param_map["guidance_scale_id"] = str(self.guidance_scale_id)
+        param_map["negative_prompt"] = str(self.negative_prompt)
+        param_map["num_inference_steps"] = str(self.num_inference_steps)
+        param_map["num_images_per_prompt"] = str(self.num_images_per_prompt)
+        param_map["seed"] = str(self.seed)
+        param_map["width"] = str(self.width)
+        param_map["height"] = str(self.height)
+        param_map["mode"] = str(self.mode)
+        param_map["id_mix"] = str(self.id_mix)
+        return param_map
+
+
+# --------------------
+# - Class which implements the algorithm
+# - Inherits PyCore.CWorkflowTask or derived from Ikomia API
+# --------------------
+class InferPulid(core.CWorkflowTask):
+
+    def __init__(self, name, param):
+        core.CWorkflowTask.__init__(self, name)
+        # Main image
+        self.add_input(dataprocess.CImageIO())
+        # Optional additional images
+        self.add_input(dataprocess.CImageIO())
+        self.add_input(dataprocess.CImageIO())
+        self.add_input(dataprocess.CImageIO())
+
+        # Create parameters object
+        if param is None:
+            self.set_param_object(InferPulidParam())
+        else:
+            self.set_param_object(copy.deepcopy(param))
+        
+        self.pipeline = None
+        self.max_seed_value = 1919655350
+        self.resize_long_edge = 1024
+
+    def get_progress_steps(self):
+        # Function returning the number of progress steps for this algorithm
+        # This is handled by the main progress bar of Ikomia Studio
+        return 1
+
+    def inference(self, img, supp_images):
+        # Get parameters
+        param = self.get_param_object()
+        
+        # Adjust attention settings based on mode
+        if param.mode == 'fidelity':
+            attention.NUM_ZERO = 8
+            attention.ORTHO = False
+            attention.ORTHO_v2 = True
+        elif param.mode == 'extremely style':
+            attention.NUM_ZERO = 16
+            attention.ORTHO = True
+            attention.ORTHO_v2 = False
+        else:
+            raise ValueError("Unsupported mode")
+
+        # Process the ID images and resize them
+        if img is not None:
+            id_image = resize_numpy_image_long(img, 1024)
+            id_embeddings = self.pipeline.get_id_embedding(id_image)
+            if supp_images is not None:
+                for supp_image in supp_images:
+                    # Process supplementary images
+                    supp_image = resize_numpy_image_long(supp_image, self.resize_long_edge)
+                    supp_id_embeddings = self.pipeline.get_id_embedding(supp_image)
+                    # Conditionally mix embeddings
+                    id_embeddings = torch.cat(
+                        (id_embeddings, supp_id_embeddings if param.id_mix else supp_id_embeddings[:, :5]), dim=1
+                    )      
+        else:
+            id_embeddings = None
+
+        # Set the random seed
+        if param.seed == -1:
+            seed = random.randint(0, self.max_seed_value)
+        else:
+            seed = param.seed
+        seed_everything(seed)
+
+        # Generate images
+        ims = []
+        for _ in range(param.num_images_per_prompt):
+            img = self.pipeline.inference(
+                param.prompt,
+                (1, param.height, param.width),
+                param.negative_prompt,
+                id_embeddings,
+                param.guidance_scale_id,
+                param.guidance_scale,
+                param.num_inference_steps
+            )[0]
+
+            ims.append(np.array(img))
+
+        print(f"Prompt:\t{param.prompt}\nSeed:\t{seed}")
+        return ims
+
+    def run(self):
+        # Main function of your algorithm
+        # Call begin_task_run() for initialization
+        self.begin_task_run()
+
+        # Get param
+        param = self.get_param_object()  
+
+       # Load pipelines
+        if self.pipeline is None:
+            self.pipeline = PuLIDPipeline()
+
+        torch.set_grad_enabled(False)
+   
+        # Get the main input image
+        image_in = self.get_input(0)
+        image = image_in.get_image() if image_in.is_data_available() else None
+
+        # Collect supplementary images from additional inputs
+        supp_images = [self.get_input(i).get_image() for i in range(1, 4) if self.get_input(i).is_data_available()]
+
+        # Execute inference with the main and supplementary images
+        results = self.inference(image, supp_images if supp_images else None)            
+
+        # Set image output
+        if len(results) > 1:
+            for i, image in enumerate(results):
+                self.add_output(dataprocess.CImageIO())
+                img = np.array(image)
+                output = self.get_output(i)
+                output.set_image(img)
+        else:
+            image = np.array(results[0])
+            output_img = self.get_output(0)
+            output_img.set_image(image)
+
+        # Step progress bar:
+        self.emit_step_progress()
+
+        # Call end_task_run to finalize process
+        self.end_task_run()
+# --------------------
+# - Factory class to build process object
+# - Inherits PyDataProcess.CTaskFactory from Ikomia API
+# --------------------
+class InferPulidFactory(dataprocess.CTaskFactory):
+
+    def __init__(self):
+        dataprocess.CTaskFactory.__init__(self)
+        # Set algorithm information/metadata here
+        self.info.name = "infer_pulid"
+        self.info.short_description = "Pure and Lightning ID customization (PuLID) is a novel " \
+                                        "tuning-free ID customization method for text-to-image generation."
+        # relative path -> as displayed in Ikomia Studio algorithm tree
+        self.info.path = "Plugins/Python/Diffusion"
+        self.info.version = "1.0.0"
+        # self.info.icon_path = "your path to a specific icon"
+        self.info.authors = "Guo, Zinan and Wu, Yanze and Chen, Zhuowei and Chen, Lang and He, Qian"
+        self.info.article = "PuLID: Pure and Lightning ID Customization via Contrastive Alignment"
+        self.info.journal = "arXiv preprint"
+        self.info.year = 2024
+        self.info.license = "Apache License 2.0"
+        # URL of documentation
+        self.info.documentation_link = "https://arxiv.org/abs/2404.16022"
+        # Code source repository
+        self.info.repository = "https://github.com/Ikomia-hub/infer_pulid"
+        self.info.original_repository = "https://github.com/ToTheBeginning/PuLID"
+        # Python version
+        self.info.min_python_version = "3.10.0"
+        # Keywords used for search
+        self.info.keywords = "Stable Diffusion, Hugging Face,text-to-image, Generative, ID Customization"
+        self.info.algo_type = core.AlgoType.INFER
+        self.info.algo_tasks = "IMAGE_GENERATION"
+
+    def create(self, param=None):
+        # Create algorithm object
+        return InferPulid(self.info.name, param)
